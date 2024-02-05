97d944e06d36e6e7985e48089d1491214a3450a1
mnenie <121057011+mneniee@users.noreply.github.com>
2024-02-05 18:01:58 +0300

    linter /*.ts

--- packages/eslint-config-custom/library.ts
+++ packages/eslint-config-custom/library.ts
@@ -1,20 +1,14 @@
+//@ts-ignore
 const { resolve } = require("node:path");
-
+//@ts-ignore
 const project = resolve(process.cwd(), "tsconfig.json");
 
-/*
- * This is a custom ESLint configuration for use with
- * typescript packages.
- *
- * This config extends the Vercel Engineering Style Guide.
- * For more information, see https://github.com/vercel/style-guide
- *
- */
 
 module.exports = {
   extends: [
     "@vercel/style-guide/eslint/node",
     "@vercel/style-guide/eslint/typescript",
+    //@ts-ignore
   ].map(require.resolve),
   parserOptions: {
     project,
